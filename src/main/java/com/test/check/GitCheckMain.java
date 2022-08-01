ba97dc9c5b2d18efc04ccc6b9820fd53848ed481
Prathamesh <prathameshm.syca@gmail.com>
2022-08-01 16:26:16 +0530

    First Test Commit

---
+++ src/main/java/com/test/check/GitCheckMain.java
@@ -0,0 +1,23 @@
+package com.test.check;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitCheckMain {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+		
+		driver.close();
+		driver.quit();
+		
+	}
+
+}
